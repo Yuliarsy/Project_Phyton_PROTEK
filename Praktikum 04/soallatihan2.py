67408f0540f74626d395885c725b0cafb35ebd87
Yulia Rakhmah Syifani <71741018+Yuliarsy@users.noreply.github.com>
2020-10-17 10:45:26 +0700

    Create soallatihan2.py

---
+++ Praktikum 04/soallatihan2.py
@@ -0,0 +1,7 @@
+#Keterangan soal
+JarakkotaAkeC= 795
+Jaraktempuhyangdiapaidengan1liter= 12
+
+Bensinyangdiperlukan= JarakkotaAkeC/Jaraktempuhyangdiapaidengan1liter
+
+print("Bensin yang diperlukan Pak Budi", Bensinyangdiperlukan)
