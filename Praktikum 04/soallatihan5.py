600a2b3b324eb2a14b07a1eca6cd80dedcd89f20
Yulia Rakhmah Syifani <71741018+Yuliarsy@users.noreply.github.com>
2020-10-17 12:01:05 +0700

    Create soallatihan5.py

---
+++ Praktikum 04/soallatihan5.py
@@ -0,0 +1,12 @@
+print("Diangram Batang Horizontal Jumlah Mahasiswa Laki Laki dan Perempuan PTIK UNS")
+
+#Input
+MahasiswaLakiLaki= int(input("Masukan Jumlah Siswa Laki Laki= "))
+MahasiswaPerempuan= int(input("Masukan Jumlah Siswa Perempuan= "))
+
+#Output
+OutputMahasiswaLakiLaki= int(MahasiswaLakiLaki//10)
+OutputMahasiswaPerempuan= int(MahasiswaPerempuan//10)
+
+print("Laki Laki =", OutputMahasiswaLakiLaki*"*", MahasiswaLakiLaki)
+print("Perempuan =", OutputMahasiswaPerempuan*"*", MahasiswaPerempuan)
