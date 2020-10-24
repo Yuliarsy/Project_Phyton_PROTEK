d0f3173e5b79cb916112a1434fd0c121c8ff3436
Yulia Rakhmah Syifani <71741018+Yuliarsy@users.noreply.github.com>
2020-10-24 14:36:48 +0700

    praktikum5

---
+++ Praktikum 05/chapter5-praktikum1.8.py
@@ -0,0 +1,20 @@
+#Kode1
+
+a=-7
+b=2
+if(a>0)and(b>0):
+    print("Keduanya positif")
+else:
+    print("Keduanya tidak positif")
+
+#Kode2
+a=-7
+b=2
+if(a>0):
+    if(b>0):
+        print("Keduanya positif")
+    else:
+        print("Keduanya tidak positif")
+else:
+    print("Keduanya tidak positif")
+
