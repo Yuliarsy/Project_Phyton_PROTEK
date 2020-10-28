97e52d43d7c79cb618dfd152956007a2cf39d704
Yulia Rakhmah Syifani <71741018+Yuliarsy@users.noreply.github.com>
2020-10-28 12:50:18 +0700

    latihan2

---
+++ Praktikum 05/chapter5-latihan2.2.py
@@ -0,0 +1,5 @@
+JumlahBilangan=0
+for a in range (1,100,2):
+    print (a)
+    JumlahBilangan+=1
+print('Jumlah Bilangan Ganjil = ', JumlahBilangan)
---
+++ Praktikum 05/chapter5-latihan2.3.py
@@ -0,0 +1,8 @@
+JumlahBilangan=0
+sum=0
+for a in range (1,100,2):
+    print (a)
+    JumlahBilangan+=1
+    sum=sum+a
+print('Jumlah Bilangan Ganjil = ', JumlahBilangan)
+print('Jumlah Seluruh Bilangan= ', sum)
